05d9a1ee789456a99d93656366a28bca1b5f1c71
Seungyeol <marudev4905@gmail.com>
2020-06-19 21:54:38 +0900

    component videoCard 개발 중

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,20 +1,21 @@
 // section Variable
 const useful = document.querySelector(".useful");
 const usefulBox = document.querySelectorAll(".useful__summary__box");
-const usefulTop = Math.round(useful.getBoundingClientRect().top);
-const usefulBoxTop = usefulBox[1].offsetHeight;
-
+const usefulBoxHeight = document.querySelector(".useful__summary").offsetHeight;
 let windowScroll;
 let windowHeight;
 
 window.addEventListener("scroll",()=>{
     windowScroll = window.scrollY;
-    windowHeight = window.innerHeight;
-    if( usefulBoxTop < windowScroll){
-        for(let i = 0; i < usefulBox.length; i++){
-            usefulBox[i].style.position="relative";
-            usefulBox[i].style.animation="1s ease forwards fade-left";
-            usefulBox[i].style.animationDelay= i * 0.25+"s";
-        }
+    
+    if( usefulBoxHeight => windowScroll){
+        createAnimation();
     }
 });
+function createAnimation(){
+    for(let i = 0; i < usefulBox.length; i++){
+        usefulBox[i].style.position="relative";
+        usefulBox[i].style.animation="1s ease forwards fade-left";
+        usefulBox[i].style.animationDelay= i * 0.25+"s";
+    }
+}
