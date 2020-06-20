7b7c66126fc0b307c1cd1b97ef9021e1da20ee92
Seungyeol <marudev4905@gmail.com>
2020-06-20 12:35:53 +0900

    animation css, js 코드 기능별로 구분

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,21 +1,37 @@
 // section Variable
 const useful = document.querySelector(".useful");
-const usefulBox = document.querySelectorAll(".useful__summary__box");
+const bannerContent = document.querySelector(".banner__content");
 const usefulBoxHeight = document.querySelector(".useful__summary").offsetHeight;
+
+const usefulBox = document.querySelectorAll(".useful__summary__box");
+const menuItem = document.querySelectorAll(".menu__item");
+
 let windowScroll;
 let windowHeight;
 
+window.addEventListener("load",()=>{
+    addClassBaner();
+});
+
 window.addEventListener("scroll",()=>{
+    scrollDetact();
+});
+
+function scrollDetact(){
     windowScroll = window.scrollY;
     
     if( usefulBoxHeight => windowScroll){
-        createAnimation();
+        addClassUseful();
     }
-});
-function createAnimation(){
-    for(let i = 0; i < usefulBox.length; i++){
-        usefulBox[i].style.position="relative";
-        usefulBox[i].style.animation="1s ease forwards fade-left";
-        usefulBox[i].style.animationDelay= i * 0.25+"s";
+}
+function addClassBaner(){
+    for(let i = 0; i < menuItem.length; i++){
+        menuItem[i].classList.add("fade");
+    }    
+    bannerContent.classList.add("fade");
+}
+function addClassUseful(){
+    for(let i = 0; i < usefulBox.length;i++){
+        usefulBox[i].classList.add("fade");
     }
 }
