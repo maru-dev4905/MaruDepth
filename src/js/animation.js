030b2ce7ea1ea2bca53bab002e30950ec8ea6268
Seungyeol <marudev4905@gmail.com>
2020-06-21 18:39:00 +0900

    animation js code 개선 및 useful section animation 적용;

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,37 +1,83 @@
-// section Variable
-const useful = document.querySelector(".useful");
-const bannerContent = document.querySelector(".banner__content");
-const usefulBoxHeight = document.querySelector(".useful__summary").offsetHeight;
+const animation = function() {
+    let 
+        // banner
+        banner,
+        bannerContent,
+        menuItem,
+        bannerTop,
 
-const usefulBox = document.querySelectorAll(".useful__summary__box");
-const menuItem = document.querySelectorAll(".menu__item");
+        // useful
+        summary,
+        usefulBox,
+        usefulTitle,
+        usefulTitleTop,
+        summaryTop,
 
-let windowScroll;
-let windowHeight;
+        // video
+        videoRows,
+        videoBox,
+        videoTop,
 
-window.addEventListener("load",()=>{
-    addClassBaner();
-});
+        winH
 
-window.addEventListener("scroll",()=>{
-    scrollDetact();
-});
+    const initModule = function() {
+        // banner section
+        banner = document.querySelector(".banner");
+        bannerContent = document.querySelector(".banner__content");
+        menuItem = document.querySelectorAll(".menu__item");
+        
+        // useful section
+        usefulBox = document.querySelectorAll(".useful__summary__box");
+        usefulTitle = document.querySelector(".useful__title");
+        summary = document.querySelector(".useful__summary");
 
-function scrollDetact(){
-    windowScroll = window.scrollY;
+        // video section
+        videoRows = document.querySelectorAll(".video__site__rows");
+        videoBox = document.querySelectorAll(".video__box");
+
+        winH = window.innerHeight;
+
+        _addEventHandlers();
+    }
+
+    const _addEventHandlers = function() {
+        window.addEventListener("scroll", _checkPosition);
+        window.addEventListener("load", _checkPosition);
+        window.addEventListener("resize", initModule);
+    }
     
-    if( usefulBoxHeight => windowScroll){
-        addClassUseful();
+    const _checkPosition = function() {
+        bannerTop = banner.getBoundingClientRect().top;        
+        summaryTop = summary.getBoundingClientRect().top;
+        usefulTitleTop = usefulTitle.getBoundingClientRect().top;
+        videoTop = videoRows[0].getBoundingClientRect().top;
+
+        while( winH > bannerTop ){
+            for(let i = 0; i < menuItem.length; i++){
+                menuItem[i].classList.add("fade");
+            }
+            bannerContent.classList.add("fade");
+            break;
+        }
+        while ( winH > summaryTop ){
+            for(let i = 0; i < usefulBox.length; i++){
+                usefulBox[i].classList.add("fade");
+            }
+            break;
+        }
+        while ( winH > usefulTitleTop ){
+            usefulTitle.classList.add("fade");
+            break;
+        }
+        while( winH > videoTop ){
+            for(let i = 0; i < videoRows.length; i++){
+                videoRows[i].classList.add("fade");
+            }
+            break;
+        }
+    }
+    return {
+        init : initModule
     }
 }
-function addClassBaner(){
-    for(let i = 0; i < menuItem.length; i++){
-        menuItem[i].classList.add("fade");
-    }    
-    bannerContent.classList.add("fade");
-}
-function addClassUseful(){
-    for(let i = 0; i < usefulBox.length;i++){
-        usefulBox[i].classList.add("fade");
-    }
-}
+animation().init();
