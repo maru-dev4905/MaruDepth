c2e5aec43bc070c24f7626687b65eae460c5889e
Seungyeol <marudev4905@gmail.com>
2020-06-25 18:07:32 +0900

    scroll js code 개선 및 banner, header tablet 반응형 구현

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,24 +1,60 @@
-const header = document.querySelector(".header");
-const nav = document.querySelector(".banner__container");
-let delta = false;
-let nowScroll;
-let lastScroll = 0;
+const scroll = function () {
+    let delta,
+        nowScroll,
+        lastScroll,
+        winWidth;
 
-window.addEventListener("scroll",()=>{
-    nowScroll = window.scrollY;
+    const header = document.querySelector(".header"),
+          nav = document.querySelector(".banner__container"),
+          TABLET_SIZE = 768;
 
-    if(nowScroll < lastScroll){
-        if(delta){
-            header.style.animation="headerShow 0.3s forwards";
-            nav.style.height="calc(100% - 170px)";
-            delta=false;
-        }
-    }else if(nowScroll > lastScroll){
-        if(delta == false){
-            header.style.animation="headerHide 0.3s forwards";
-            nav.style.height="calc(100% - 120px)";
-            delta=true;
-        }
+    const initModule = function () {
+        delta = false;
+        lastScroll = 0;
+        winWidth = window.innerWidth;
+
+        _addEventHandlers();
     }
-    lastScroll = window.scrollY;
-});
+
+    const _addEventHandlers = function () {
+        window.addEventListener("scroll", _checkPosition);
+        window.addEventListener("load", _checkPosition);
+        window.addEventListener("resize", initModule);
+    }
+
+    const _checkPosition = function () {
+        if(winWidth < TABLET_SIZE){
+            return false;
+        }else if(winWidth > TABLET_SIZE){
+            _changeHeaderPosition();
+        }
+
+    }
+    const _changeHeaderPosition = function () {
+        nowScroll = window.scrollY;
+
+        if (nowScroll < lastScroll) {
+            if (delta) {
+                header.classList.remove("hide")
+                nav.classList.remove("hide");
+                header.classList.add("show");
+                nav.classList.add("show");
+                delta = false;
+            }
+        } else if (nowScroll > lastScroll) {
+            if (delta == false) {
+                header.classList.remove("show")
+                nav.classList.remove("show");
+                header.classList.add("hide");
+                nav.classList.add("hide");
+                delta = true;
+            }
+        }
+        lastScroll = window.scrollY;
+    };
+    return {
+        init : initModule
+    }
+
+}
+scroll().init();
