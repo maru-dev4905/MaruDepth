cd37e2efc8539c86426a021354df49d63d728062
maru <marudev4905@gmail.com>
2020-09-16 11:29:25 +0900

    서브페이지에서 카테고리 클릭 시 메인페이지로 이동 기능 추가

--- src/js/clickCategory.js
+++ src/js/clickCategory.js
@@ -22,67 +22,87 @@ devStudyBtn.addEventListener("click",()=>{
             create__card(name,text,link,tags);
         }
     }else{
-        window.location.href = "index.html"
+        window.location.href = "index.html";
     }
 });
 
 devCommunityBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < devJson.community.length; i++){
-        let name = devJson.community[i].siteName;
-        let text = devJson.community[i].siteText;
-        let link = devJson.community[i].siteLink;
-        let tags = devJson.community[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < devJson.community.length; i++){
+            let name = devJson.community[i].siteName;
+            let text = devJson.community[i].siteText;
+            let link = devJson.community[i].siteLink;
+            let tags = devJson.community[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 devSimulationBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < devJson.simulation.length; i++){
-        let name = devJson.simulation[i].siteName;
-        let text = devJson.simulation[i].siteText;
-        let link = devJson.simulation[i].siteLink;
-        let tags = devJson.simulation[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < devJson.simulation.length; i++){
+            let name = devJson.simulation[i].siteName;
+            let text = devJson.simulation[i].siteText;
+            let link = devJson.simulation[i].siteLink;
+            let tags = devJson.simulation[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 devBlogBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < devJson.blog.length; i++){
-        let name = devJson.blog[i].siteName;
-        let text = devJson.blog[i].siteText;
-        let link = devJson.blog[i].siteLink;
-        let tags = devJson.blog[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < devJson.blog.length; i++){
+            let name = devJson.blog[i].siteName;
+            let text = devJson.blog[i].siteText;
+            let link = devJson.blog[i].siteLink;
+            let tags = devJson.blog[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 devTipBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < devJson.tip.length; i++){
-        let name = devJson.tip[i].siteName;
-        let text = devJson.tip[i].siteText;
-        let link = devJson.tip[i].siteLink;
-        let tags = devJson.tip[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < devJson.tip.length; i++){
+            let name = devJson.tip[i].siteName;
+            let text = devJson.tip[i].siteText;
+            let link = devJson.tip[i].siteLink;
+            let tags = devJson.tip[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 devEtcBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < devJson.etc.length; i++){
-        let name = devJson.etc[i].siteName;
-        let text = devJson.etc[i].siteText;
-        let link = devJson.etc[i].siteLink;
-        let tags = devJson.etc[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < devJson.etc.length; i++){
+            let name = devJson.etc[i].siteName;
+            let text = devJson.etc[i].siteText;
+            let link = devJson.etc[i].siteLink;
+            let tags = devJson.etc[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
@@ -98,74 +118,98 @@ const designEtcBtn = document.getElementById("designEtcBtn");
 
 // design
 designImageBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < designJson.image.length; i++){
-        let name = designJson.image[i].siteName;
-        let text = designJson.image[i].siteText;
-        let link = designJson.image[i].siteLink;
-        let tags = designJson.image[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < designJson.image.length; i++){
+            let name = designJson.image[i].siteName;
+            let text = designJson.image[i].siteText;
+            let link = designJson.image[i].siteLink;
+            let tags = designJson.image[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 designIconBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < designJson.icon.length; i++){
-        let name = designJson.icon[i].siteName;
-        let text = designJson.icon[i].siteText;
-        let link = designJson.icon[i].siteLink;
-        let tags = designJson.icon[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < designJson.icon.length; i++){
+            let name = designJson.icon[i].siteName;
+            let text = designJson.icon[i].siteText;
+            let link = designJson.icon[i].siteLink;
+            let tags = designJson.icon[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 designColorBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < designJson.color.length; i++){
-        let name = designJson.color[i].siteName;
-        let text = designJson.color[i].siteText;
-        let link = designJson.color[i].siteLink;
-        let tags = designJson.color[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < designJson.color.length; i++){
+            let name = designJson.color[i].siteName;
+            let text = designJson.color[i].siteText;
+            let link = designJson.color[i].siteLink;
+            let tags = designJson.color[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 designFontBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < designJson.font.length; i++){
-        let name = designJson.font[i].siteName;
-        let text = designJson.font[i].siteText;
-        let link = designJson.font[i].siteLink;
-        let tags = designJson.font[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < designJson.font.length; i++){
+            let name = designJson.font[i].siteName;
+            let text = designJson.font[i].siteText;
+            let link = designJson.font[i].siteLink;
+            let tags = designJson.font[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 designTipBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < designJson.tip.length; i++){
-        let name = designJson.tip[i].siteName;
-        let text = designJson.tip[i].siteText;
-        let link = designJson.tip[i].siteLink;
-        let tags = designJson.tip[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < designJson.tip.length; i++){
+            let name = designJson.tip[i].siteName;
+            let text = designJson.tip[i].siteText;
+            let link = designJson.tip[i].siteLink;
+            let tags = designJson.tip[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 designEtcBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < designJson.etc.length; i++){
-        let name = designJson.etc[i].siteName;
-        let text = designJson.etc[i].siteText;
-        let link = designJson.etc[i].siteLink;
-        let tags = designJson.etc[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < designJson.etc.length; i++){
+            let name = designJson.etc[i].siteName;
+            let text = designJson.etc[i].siteText;
+            let link = designJson.etc[i].siteLink;
+            let tags = designJson.etc[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
@@ -177,38 +221,50 @@ const etcMusicBtn = document.getElementById("etcMusicBtn");
 const etcTipBtn = document.getElementById("etcTipBtn");
 
 etcPptBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < etcJson.ppt.length; i++){
-        let name = etcJson.ppt[i].siteName;
-        let text = etcJson.ppt[i].siteText;
-        let link = etcJson.ppt[i].siteLink;
-        let tags = etcJson.ppt[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < etcJson.ppt.length; i++){
+            let name = etcJson.ppt[i].siteName;
+            let text = etcJson.ppt[i].siteText;
+            let link = etcJson.ppt[i].siteLink;
+            let tags = etcJson.ppt[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.hmtl";
     }
 });
 
 etcMusicBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < etcJson.music.length; i++){
-        let name = etcJson.music[i].siteName;
-        let text = etcJson.music[i].siteText;
-        let link = etcJson.music[i].siteLink;
-        let tags = etcJson.music[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < etcJson.music.length; i++){
+            let name = etcJson.music[i].siteName;
+            let text = etcJson.music[i].siteText;
+            let link = etcJson.music[i].siteLink;
+            let tags = etcJson.music[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
 etcTipBtn.addEventListener("click",()=>{
-    remove__card();
-    for(let i = 0; i < etcJson.tip.length; i++){
-        let name = etcJson.tip[i].siteName;
-        let text = etcJson.tip[i].siteText;
-        let link = etcJson.tip[i].siteLink;
-        let tags = etcJson.tip[i].siteTags;
-
-        create__card(name,text,link,tags);
+    if(path === "index.html"){
+        remove__card();
+        for(let i = 0; i < etcJson.tip.length; i++){
+            let name = etcJson.tip[i].siteName;
+            let text = etcJson.tip[i].siteText;
+            let link = etcJson.tip[i].siteLink;
+            let tags = etcJson.tip[i].siteTags;
+    
+            create__card(name,text,link,tags);
+        }
+    }else{
+        window.location.href = "index.html";
     }
 });
 
--- src/js/showSite.js
+++ src/js/showSite.js
@@ -1,116 +1,118 @@
-const path = window.location.pathname.split("/").pop().toLowerCase();
 
-if(path === "index.html"){
-    const devJson = require("../js/json/dev.json");
-    const designJson = require("../js/json/design.json");
-    const etcJson = require("../js/json/etc.json");
-    
-    const siteGroup = document.querySelector("ul.site__rows");
-    
-    for (const key in devJson) {
-        for (let i = 0; i < devJson[key].length; i++) {
-    
-            let name = devJson[key][i].siteName;
-            let text = devJson[key][i].siteText;
-            let link = devJson[key][i].siteLink;
-            let tags = devJson[key][i].siteTags;
-    
-            create__card(name, text, link, tags);
+window.addEventListener("load",()=>{
+    const path = window.location.pathname.split("/").pop();
+    if(path === "index.html"){
+        const devJson = require("../js/json/dev.json");
+        const designJson = require("../js/json/design.json");
+        const etcJson = require("../js/json/etc.json");
+        
+        const siteGroup = document.querySelector("ul.site__rows");
+        
+        for (const key in devJson) {
+            for (let i = 0; i < devJson[key].length; i++) {
+        
+                let name = devJson[key][i].siteName;
+                let text = devJson[key][i].siteText;
+                let link = devJson[key][i].siteLink;
+                let tags = devJson[key][i].siteTags;
+        
+                create__card(name, text, link, tags);
+            }
+        }
+        
+        for (const key in designJson) {
+            for (let i = 0; i < designJson[key].length; i++) {
+        
+                let name = designJson[key][i].siteName;
+                let text = designJson[key][i].siteText;
+                let link = designJson[key][i].siteLink;
+                let tags = designJson[key][i].siteTags;
+        
+                create__card(name, text, link, tags);
+            }
+        }
+        
+        for (const key in etcJson) {
+            for (let i = 0; i < etcJson[key].length; i++) {
+        
+                let name = etcJson[key][i].siteName;
+                let text = etcJson[key][i].siteText;
+                let link = etcJson[key][i].siteLink;
+                let tags = etcJson[key][i].siteTags;
+        
+                create__card(name, text, link, tags);
+            }
+        }
+        function create__card(name, text, link, tags) {
+        
+            const li = document.createElement("li");
+        
+            li
+                .classList
+                .add("site-item");
+        
+            const imgBox = document.createElement("div");
+            imgBox
+                .classList
+                .add("site__img");
+        
+            const a1 = document.createElement("a");
+            a1.setAttribute("href", link);
+            a1.setAttribute("target", "_blank");
+        
+            const img = document.createElement("img");
+            img
+                .classList
+                .add("site--img");
+            img.setAttribute("src", "./images/common/thumbnail/" + name + ".png");
+            img.setAttribute("alt", name + "-img");
+        
+            const contentBox = document.createElement("div");
+            contentBox
+                .classList
+                .add("site__content");
+        
+            const strong = document.createElement("strong");
+            strong
+                .classList
+                .add("site--title");
+            strong.innerText = name;
+        
+            const p = document.createElement("p");
+            p
+                .classList
+                .add("site--desc");
+            p.innerText = text;
+        
+            const a2 = document.createElement("a");
+            a2
+                .classList
+                .add("site--link");
+            a2.setAttribute("href", link);
+            a2.setAttribute("target", "_blank");
+            a2.innerText = "Go to Site";
+        
+            const span = document.createElement("span");
+            span
+                .classList
+                .add("sr");
+            span
+                .classList
+                .add("site--tags");
+            span.innerText = tags;
+        
+            contentBox.appendChild(strong);
+            contentBox.appendChild(p);
+            contentBox.appendChild(a2);
+            contentBox.appendChild(span);
+        
+            a1.appendChild(img);
+            imgBox.appendChild(a1);
+        
+            li.appendChild(imgBox);
+            li.appendChild(contentBox);
+        
+            siteGroup.appendChild(li);
         }
     }
-    
-    for (const key in designJson) {
-        for (let i = 0; i < designJson[key].length; i++) {
-    
-            let name = designJson[key][i].siteName;
-            let text = designJson[key][i].siteText;
-            let link = designJson[key][i].siteLink;
-            let tags = designJson[key][i].siteTags;
-    
-            create__card(name, text, link, tags);
-        }
-    }
-    
-    for (const key in etcJson) {
-        for (let i = 0; i < etcJson[key].length; i++) {
-    
-            let name = etcJson[key][i].siteName;
-            let text = etcJson[key][i].siteText;
-            let link = etcJson[key][i].siteLink;
-            let tags = etcJson[key][i].siteTags;
-    
-            create__card(name, text, link, tags);
-        }
-    }
-    function create__card(name, text, link, tags) {
-    
-        const li = document.createElement("li");
-    
-        li
-            .classList
-            .add("site-item");
-    
-        const imgBox = document.createElement("div");
-        imgBox
-            .classList
-            .add("site__img");
-    
-        const a1 = document.createElement("a");
-        a1.setAttribute("href", link);
-        a1.setAttribute("target", "_blank");
-    
-        const img = document.createElement("img");
-        img
-            .classList
-            .add("site--img");
-        img.setAttribute("src", "./images/common/thumbnail/" + name + ".png");
-        img.setAttribute("alt", name + "-img");
-    
-        const contentBox = document.createElement("div");
-        contentBox
-            .classList
-            .add("site__content");
-    
-        const strong = document.createElement("strong");
-        strong
-            .classList
-            .add("site--title");
-        strong.innerText = name;
-    
-        const p = document.createElement("p");
-        p
-            .classList
-            .add("site--desc");
-        p.innerText = text;
-    
-        const a2 = document.createElement("a");
-        a2
-            .classList
-            .add("site--link");
-        a2.setAttribute("href", link);
-        a2.setAttribute("target", "_blank");
-        a2.innerText = "Go to Site";
-    
-        const span = document.createElement("span");
-        span
-            .classList
-            .add("sr");
-        span
-            .classList
-            .add("site--tags");
-        span.innerText = tags;
-    
-        contentBox.appendChild(strong);
-        contentBox.appendChild(p);
-        contentBox.appendChild(a2);
-        contentBox.appendChild(span);
-    
-        a1.appendChild(img);
-        imgBox.appendChild(a1);
-    
-        li.appendChild(imgBox);
-        li.appendChild(contentBox);
-    
-        siteGroup.appendChild(li);
-    }
-}
+})
